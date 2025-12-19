925989fa946dfd29f1960d6418a67ee0f1be7e01
Tianyi Pu <912396513@qq.com>
2025-12-19 21:54:02 +0100

    support `==`

--- src/Z2LinearAlgebra/algebra.jl
+++ src/Z2LinearAlgebra/algebra.jl
@@ -1,4 +1,8 @@
 
+==(u::Z2Vector, v::Z2Vector) = u.size == v.size && u.data == v.data
+==(u::Z2ColMat, v::Z2ColMat) = u.size == v.size && u.data == v.data
+==(u::Z2RowMat, v::Z2RowMat) = u.size == v.size && u.data == v.data
+
 function +(u::Z2Vector{T}, v::Z2Vector{T}) where T
     promote_shape(u, v)
     unsafe_Z2Vector(u.data ⊻ v.data, u.size)
