c5039c64b2989211a649af3780149ee9ceb59acb
Tianyi Pu <912396513@qq.com>
2025-12-22 07:53:59 +0100

    Update Z2LinearAlgebra.jl

--- src/Z2LinearAlgebra/Z2LinearAlgebra.jl
+++ src/Z2LinearAlgebra/Z2LinearAlgebra.jl
@@ -1,5 +1,5 @@
 import Base: *, +, -, &, |, ⊻, ⊼, ⊽, ~, \
-import Base: size, zero, fill!, getindex, error_if_canonical_getindex, unsafe_getindex, promote_shape, setindex!, copy, transpose, adjoint, similar, one, IndexStyle
+import Base: size, zero, fill!, getindex, error_if_canonical_getindex, unsafe_getindex, promote_shape, setindex!, copy, transpose, adjoint, similar, one, IndexStyle, @propagate_inbounds
 import Base: sum, prod, any
 import Base: UndefInitializer, Dims
 import LinearAlgebra: dot, AdjOrTrans, mul!, rank, det, checksquare, ldiv!, SingularException, lmul!, rmul!
